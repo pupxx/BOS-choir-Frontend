0c8d1d9fb45d353110e9b95a0128c15a8e1146da
Cheryln <cherylnbarber@gmail.com>
2017-11-15 18:30:14 -0800

    Fixed spelling mistake on className. Fixed bug with back drop not disappearing on larger screen.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import classes from './about.css'
 
 const About = function () {
 
     return (
-        <div>
-            <div>
-                <h2>Barrie Ontario Stake Choir</h2>
-                <div className="photo"></div>
-                <div className="about">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam blanditiis exercitationem placeat commodi, dicta ipsa perspiciatis architecto enim amet eos eligendi in eveniet. Provident neque at dolorem officiis enim cumque.</p>
-                </div>
+        <div className={classes.AboutWrapper}>
+            <h2>Barrie Ontario Stake Choir</h2>
+            <div className="photo"></div>
+            <div className="about">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam blanditiis exercitationem placeat commodi, dicta ipsa perspiciatis architecto enim amet eos eligendi in eveniet. Provident neque at dolorem officiis enim cumque.</p>
             </div>
         </div>
     )
