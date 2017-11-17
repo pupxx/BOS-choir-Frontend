11a5bb9c3ba10a93d2c41ec508b0bedd44cde361
Cheryln <cherylnbarber@gmail.com>
2017-11-17 10:58:16 -0800

    Fixed Link to rehearsals.

--- src/components/navigation/navigationItem/NavigationItem.js
+++ src/components/navigation/navigationItem/NavigationItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 import classes from './navigationItem.css';
@@ -8,8 +8,8 @@ import classes from './navigationItem.css';
 function navigationItem(props) {
     return (
         <li className={classes.NavigationItem}>
-            <a to={props.link}
-            className={props.active ? classes.active : null}>{props.children} <div>></div></a>
+            <Link to={props.link}
+            className={props.active ? classes.active : null}>{props.children} <div>></div></Link>
         </li>
     )
 }
