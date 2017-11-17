a081f14b021fd501243be159d0a011622c3f05d5
Cheryln <cherylnbarber@gmail.com>
2017-11-17 07:19:39 -0800

    Added first main route.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import Layout from '../layout/Layout';
 import About from '../about/About'
 import PerformanceList from '../../containers/performance_list/PerformanceList';
@@ -10,8 +12,8 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <About />
-          <PerformanceList />
+            <Route path="/" component={About} />
+            <Route path="/" component={PerformanceList} />
         </Layout>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 
 import './index.css';
 import App from './components/app/App';
+import About from './components/about/About';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './store/reducers';
 
@@ -15,7 +16,9 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 const app = (
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
-            <App />
+        <div>
+            <Route path="/" component={App}/>
+        </div>
         </BrowserRouter>
     </Provider>
 )
