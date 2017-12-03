92dc668253ec75448b219668933dbcb9fafd8fc0
Cheryln <cherylnbarber@gmail.com>
2017-12-03 15:46:19 -0800

    Added signup component. It does not call the back end yet but form is created and rendering to the page.

--- src/components/auth/signup/Signup.js
+++ src/components/auth/signup/Signup.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+
+import classes from "./signup.css";
 
 class Signup extends Component {
+  onSubmit(values) {
+    console.log("onsubmit");
+  }
+
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input {...field.input} className="form-control" type={field.type} />
+      </div>
+    );
+  }
+
   render() {
-    return <div>Hello</div>;
+    const { handleSubmit } = this.props;
+    const signupButtonStyle = `${
+      classes.SignupButton
+    } btn btn-primary btn-sm btn-block`;
+    return (
+      <form
+        className={classes.Form}
+        onSubmit={handleSubmit(this.onSubmit.bind(this))}
+      >
+        <Field
+          name="email"
+          label="Email: "
+          type="email"
+          component={this.renderField}
+        />
+        <Field
+          name="confirmEmail"
+          label="Confirm Email:"
+          type="email"
+          component={this.renderField}
+        />
+        <Field
+          name="password"
+          label="Password:"
+          type="password"
+          component={this.renderField}
+        />
+        <Link to="/signup" className={signupButtonStyle}>
+          Register
+        </Link>
+      </form>
+    );
   }
 }
 
-export default connect(null, null)(Signup);
+export default reduxForm({
+  form: "signup"
+})(connect(null, null)(Signup));
