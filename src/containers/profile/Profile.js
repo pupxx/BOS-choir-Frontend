90ca2660f0c76747aa4af427666a477b54f965a2
Cheryln <cherylnbarber@gmail.com>
2017-12-04 15:39:34 -0800

    Started creating the profile Component.

---
+++ src/containers/profile/Profile.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Profile extends Component {
+  render() {
+    return <div>Profile Page</div>;
+  }
+}
+
+export default connect(null, null)(Profile);
