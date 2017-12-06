8927dd87ecd3672d76de2484553250af1be169e5
Cheryln <cherylnbarber@gmail.com>
2017-12-06 14:08:06 -0800

    changed rehearsal default state to be and object.

--- src/store/reducers/reducer_rehearsals.js
+++ src/store/reducers/reducer_rehearsals.js
@@ -1,7 +1,7 @@
 import { FETCH_REHEARSALS } from "../actions/types";
 import _ from "lodash";
 
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_REHEARSALS:
       console.log(action.payload);
