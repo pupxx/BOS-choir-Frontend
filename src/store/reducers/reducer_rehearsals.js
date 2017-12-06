cb0d53e685b9824f96e79949e91de0bc79b3475c
Cheryln <cherylnbarber@gmail.com>
2017-12-06 13:53:48 -0800

    Finished refactoring rehearsals state to be an object (converting from the incoming array using lodash.).

--- src/store/reducers/reducer_rehearsals.js
+++ src/store/reducers/reducer_rehearsals.js
@@ -1,10 +1,11 @@
 import { FETCH_REHEARSALS } from "../actions/types";
 import _ from "lodash";
 
-export default (state = {}, action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case FETCH_REHEARSALS:
-      return _.mapKeys(action.payload, "id");
+      console.log(action.payload);
+      return _.mapKeys(action.payload, "rehearsalID");
     default:
       return state;
   }
