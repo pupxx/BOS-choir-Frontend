2b600a10fa8a452d682407f4cc1168d33455ed0d
Cheryln <cherylnbarber@gmail.com>
2017-12-06 13:42:01 -0800

    Working on refactoring the rehearsals state to be an object.

--- src/containers/rehearsals/Rehearsals.js
+++ src/containers/rehearsals/Rehearsals.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchRehearsals } from '../../store/actions/index';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import _ from "lodash";
+import { fetchRehearsals } from "../../store/actions/index";
 
-class Rehearsal extends Component { 
-    constructor(props) {
-        super(props)
-        
-        this.listRehearsals = this.listRehearsals.bind(this);
-    }
-       
-    componentDidMount() {
-        this.props.fetchRehearsals()
-    }
-    
-    listRehearsals(){
-        return this.props.rehearsals.map((el)=>{
-            return (
-            <div key={el.rehearsedate}>
-                <p>{el.rehearsedate}</p> 
-                <p>{el.churchname}</p>
-                <p>{el.churchphone}</p>
-            </div>
-            )   
-        })
-    }
+class Rehearsal extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <div>{this.listRehearsals()}</div>
-        );
-    }
+    this.listRehearsals = this.listRehearsals.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchRehearsals();
+  }
+
+  listRehearsals() {
+    return _.map(this.props.rehearsals, el => {
+      return (
+        <div key={el.rehearsedate}>
+          <p>{el.rehearsedate}</p>
+          <p>{el.churchname}</p>
+          <p>{el.churchphone}</p>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div>{this.listRehearsals()}</div>;
+  }
 }
 
 function mapStateToProps(state) {
-    return { rehearsals: state.rehearsals }
+  return { rehearsals: state.rehearsals };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ fetchRehearsals }, dispatch)
+  return bindActionCreators({ fetchRehearsals }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Rehearsal);
--- src/store/reducers/reducer_rehearsals.js
+++ src/store/reducers/reducer_rehearsals.js
@@ -1,10 +1,10 @@
 import { FETCH_REHEARSALS } from "../actions/types";
+import _ from "lodash";
 
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_REHEARSALS:
-      console.log(action.payload.data);
-      return action.payload;
+      return _.mapKeys(action.payload, "id");
     default:
       return state;
   }
