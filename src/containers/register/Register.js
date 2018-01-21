bf91efc89d2f1c8ec161e4286def5572f5520bdd
Cheryln <cherylnbarber@gmail.com>
2018-01-21 12:09:44 -0800

    Added more form fields to register form. Still working on the select type of input.

--- src/containers/register/Register.js
+++ src/containers/register/Register.js
@@ -7,35 +7,51 @@ import classes from "./register.css";
 class Register extends Component {
   renderField(field) {
     return (
-      <div>
+      <div className="form-group">
         <label className={classes.Label}>{field.label}</label>
-        <input {...field.input} type={field.type} />
+        <input
+          {...field.input}
+          className="form-control"
+          type={field.type}
+          placeholder={field.placeholder}
+        />
       </div>
     );
   }
 
-  renderProvinceOptions(field) {
+  renderSelect(field) {
     return (
-      <div>
+      <div className="form-group">
         <label>{field.label}</label>
-        <select type="select">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
+        <select {...field.input} type="select" className="form-control">
+          <option value="">select</option>
+          <option value="soprano">Soprano</option>
+          <option value="alto">Alto</option>
+          <option value="tenor">Tenor</option>
+          <option value="bass">Base</option>
         </select>
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    console.log(this.props);
+    const { handleSubmit } = this.props;
     return (
-      <form className={classes.Form}>
+      <form
+        onSubmit={handleSubmit(this.onSubmit.bind(this))}
+        className={classes.Form}
+      >
         <h3>Please Register</h3>
         <Field
-          name="firstName:"
-          label="First Name"
+          name="firstName"
+          label="First Name:"
           type="text"
+          placeholder="First Name"
           component={this.renderField}
         />
         <Field
@@ -68,11 +84,42 @@ class Register extends Component {
           type="text"
           component={this.renderField}
         />
+        <Field
+          name="phone"
+          label="Phone:"
+          type="text"
+          component={this.renderField}
+        />
+        <Field
+          name="email"
+          label="Email:"
+          type="email"
+          component={this.renderField}
+        />
+        <Field
+          label="Ward or Branch"
+          name="ward"
+          component={this.renderSelect}
+          className="form-control"
+        />
+        <Field
+          label="Part"
+          name="part"
+          component={this.renderSelect}
+          className="form-control"
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {};
+}
+
 export default reduxForm({
   form: "register"
-})(connect(null, null)(Register));
+})(connect(mapStateToProps, null)(Register));
