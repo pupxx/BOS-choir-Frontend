bbc58a14d63660e5c28f67d528a06cb09b94a376
Cheryln <cherylnbarber@gmail.com>
2018-02-09 16:56:03 -0800

    More flexibility with having a separate table for each topic. ie) members, performances etc. Moved table to AdminmemberList and data is showing up on the page as expected.

--- src/containers/admin/adminMemberList/AdminMemberList.js
+++ src/containers/admin/adminMemberList/AdminMemberList.js
@@ -1,19 +1,117 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../../store/actions";
+import { Table, Form } from "semantic-ui-react";
+import LoaderWithText from "../../../components/UI/loaders/LoaderWithText";
+import _ from "lodash";
 
-import SearchableTable from "../searchableTable/SearchableTable";
+import classes from "./adminMemberList.css";
+
+// import SearchableTable from "../searchableTable/SearchableTable";
 
 class AdminMemberList extends Component {
-  state = {};
+  state = {
+    items: _.map(this.props.adminMemberList),
+    column: null,
+    direction: null
+  };
 
   componentWillMount() {
     this.props.fetchAdminMemberList();
   }
 
+  componentWillReceiveProps() {
+    this.setState({ items: _.map(this.props.adminMemberList) });
+  }
+
+  handleChange(event) {
+    var updatedList = _.map(this.props.adminMemberList);
+    updatedList = updatedList.filter(function(item) {
+      return (
+        item.lastname.toLowerCase().search(event.target.value.toLowerCase()) !==
+        -1
+      );
+    });
+    this.setState({ items: updatedList });
+  }
+
+  handleSort = clickedColumn => () => {
+    const { column, items, direction } = this.state;
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        items: _.sortBy(items, [clickedColumn]),
+        direction: "ascending"
+      });
+      return;
+    }
+    this.setState({
+      items: items.reverse(),
+      direction: direction === "ascending" ? "descending" : "ascending"
+    });
+  };
+
   render() {
-    console.log(this.props.adminMemberList);
-    return <SearchableTable />;
+    const { column, items, direction } = this.state;
+    if (!_.map(this.props.adminMemberList).length) {
+      return (
+        <div>
+          <LoaderWithText />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <hr />
+          <Form className={classes.Form}>
+            <div className="ui mini input labeled">
+              <label className="ui label label">Search Name</label>
+              <input
+                type="text"
+                placeholder="Search"
+                onChange={this.handleChange.bind(this)}
+              />
+            </div>
+          </Form>
+          <Table
+            className="ui small compact striped celled fixed sortable table"
+            color={"yellow"}
+          >
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell
+                  sorted={column === "firstname" ? direction : null}
+                  onClick={this.handleSort("firstname")}
+                >
+                  First Name
+                </Table.HeaderCell>
+                <Table.HeaderCell
+                  sorted={column === "lastname" ? direction : null}
+                  onClick={this.handleSort("lastname")}
+                >
+                  Last Name
+                </Table.HeaderCell>
+                <Table.HeaderCell
+                  sorted={column === "email" ? direction : null}
+                  onClick={this.handleSort("email")}
+                >
+                  Email
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {_.map(items, ({ memberID, firstname, lastname, email }) => (
+                <Table.Row key={memberID}>
+                  <Table.Cell>{firstname}</Table.Cell>
+                  <Table.Cell>{lastname}</Table.Cell>
+                  <Table.Cell>{email}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      );
+    }
   }
 }
 function mapstateToProps(state) {
--- src/containers/admin/searchableTable/SearchableTable.js
+++ src/containers/admin/searchableTable/SearchableTable.js
@@ -6,105 +6,105 @@ import _ from "lodash";
 import classes from "./searchableTable.css";
 
 class SearchableTable extends Component {
-  state = {
-    initialItems: [
-      { name: "John", age: 15, gender: "Male" },
-      { name: "Amber", age: 40, gender: "Female" },
-      { name: "Leslie", age: 25, gender: "Female" },
-      { name: "Ben", age: 70, gender: "Male" }
-    ],
-    items: [],
-    column: null,
-    direction: null
-  };
+  // state = {
+  //   initialItems: [
+  //     { name: "John", age: 15, gender: "Male" },
+  //     { name: "Amber", age: 40, gender: "Female" },
+  //     { name: "Leslie", age: 25, gender: "Female" },
+  //     { name: "Ben", age: 70, gender: "Male" }
+  //   ],
+  //   items: [],
+  //   column: null,
+  //   direction: null
+  // };
 
   componentWillMount() {
     this.setState({ items: this.state.initialItems });
   }
 
-  handleChange(event) {
-    var updatedList = this.state.initialItems;
-    updatedList = updatedList.filter(function(item) {
-      return (
-        item.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1
-      );
-    });
-    this.setState({ items: updatedList });
-  }
+  // handleChange(event) {
+  //   var updatedList = this.state.initialItems;
+  //   updatedList = updatedList.filter(function(item) {
+  //     return (
+  //       item.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1
+  //     );
+  //   });
+  //   this.setState({ items: updatedList });
+  // }
 
-  handleSort = clickedColumn => () => {
-    const { column, items, direction } = this.state;
+  // handleSort = clickedColumn => () => {
+  //   const { column, items, direction } = this.state;
 
-    if (column !== clickedColumn) {
-      this.setState({
-        column: clickedColumn,
-        items: _.sortBy(items, [clickedColumn]),
-        direction: "ascending"
-      });
+  //   if (column !== clickedColumn) {
+  //     this.setState({
+  //       column: clickedColumn,
+  //       items: _.sortBy(items, [clickedColumn]),
+  //       direction: "ascending"
+  //     });
 
-      return;
-    }
+  //     return;
+  //   }
 
-    this.setState({
-      items: items.reverse(),
-      direction: direction === "ascending" ? "descending" : "ascending"
-    });
-  };
+  //   this.setState({
+  //     items: items.reverse(),
+  //     direction: direction === "ascending" ? "descending" : "ascending"
+  //   });
+  // };
 
-  render() {
-    const { column, items, direction } = this.state;
-    return (
-      <div>
-        <hr />
-        <Form className={classes.Form}>
-          <div className="ui mini input labeled">
-            <label className="ui label label">Search Name</label>
-            <input
-              type="text"
-              placeholder="Search"
-              onChange={this.handleChange.bind(this)}
-            />
-          </div>
-        </Form>
-        <Table
-          className="ui small compact striped celled fixed sortable table"
-          color={"yellow"}
-        >
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell
-                sorted={column === "name" ? direction : null}
-                onClick={this.handleSort("name")}
-              >
-                Name
-              </Table.HeaderCell>
-              <Table.HeaderCell
-                sorted={column === "age" ? direction : null}
-                onClick={this.handleSort("age")}
-              >
-                Age
-              </Table.HeaderCell>
-              <Table.HeaderCell
-                sorted={column === "gender" ? direction : null}
-                onClick={this.handleSort("gender")}
-              >
-                Gender
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {items.map(item => (
-              <Table.Row key={item.name}>
-                <Table.Cell>{item.name}</Table.Cell>
-                <Table.Cell>{item.age}</Table.Cell>
-                <Table.Cell>{item.gender}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    );
-  }
+  // render() {
+  //   const { column, items, direction } = this.state;
+  //   return (
+  //     <div>
+  //       <hr />
+  //       <Form className={classes.Form}>
+  //         <div className="ui mini input labeled">
+  //           <label className="ui label label">Search Name</label>
+  //           <input
+  //             type="text"
+  //             placeholder="Search"
+  //             onChange={this.handleChange.bind(this)}
+  //           />
+  //         </div>
+  //       </Form>
+  //       <Table
+  //         className="ui small compact striped celled fixed sortable table"
+  //         color={"yellow"}
+  //       >
+  //         <Table.Header>
+  //           <Table.Row>
+  //             <Table.HeaderCell
+  //               sorted={column === "name" ? direction : null}
+  //               onClick={this.handleSort("name")}
+  //             >
+  //               Name
+  //             </Table.HeaderCell>
+  //             <Table.HeaderCell
+  //               sorted={column === "age" ? direction : null}
+  //               onClick={this.handleSort("age")}
+  //             >
+  //               Age
+  //             </Table.HeaderCell>
+  //             <Table.HeaderCell
+  //               sorted={column === "gender" ? direction : null}
+  //               onClick={this.handleSort("gender")}
+  //             >
+  //               Gender
+  //             </Table.HeaderCell>
+  //           </Table.Row>
+  //         </Table.Header>
+  //         <Table.Body>
+  //           {items.map(item => (
+  //             <Table.Row key={item.name}>
+  //               <Table.Cell>{item.name}</Table.Cell>
+  //               <Table.Cell>{item.age}</Table.Cell>
+  //               <Table.Cell>{item.gender}</Table.Cell>
+  //             </Table.Row>
+  //           ))}
+  //         </Table.Body>
+  //       </Table>
+  //     </div>
+  //   );
+  // }
 }
 
 export default connect(null, null)(SearchableTable);
